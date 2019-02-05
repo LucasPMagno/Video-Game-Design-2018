f114ee040059bd55a0d9af45533eba67c1400128
Ethan <ethanspasciani@gmail.com>
2019-02-05 18:55:49 -0500

    first commit

---
+++ Video Game Design/Assets/Start.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using Unity Engine.SceneManagement;
+
+public class Start : MonoBehaviour
+{
+   public void StartGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
